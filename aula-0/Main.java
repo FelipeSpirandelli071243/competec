0f52f6ab9f4a7526f3031af1561b746c0762cd78
FelipeSpirandelli071243 <f.spirandelli07@gmail.com>
2025-04-26 11:22:52 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	   
+	   char inicial ='f';
+	   int idade = 15;
+	   boolean cafe = true;
+	   
+	  
+	   //int uva = 6;
+	   //int pera = 4;
+	  // int multi =uva * pera;
+	   	//System.out.println("resultado = " + multi);
+	
+	
+    	Scanner  entrada = new Scanner(System.in);
+    	
+    	System.out.print("Digite o valor da pera:" );
+    	int pera = entrada.nextInt();
+    	
+    	System.out.print("Digite o valor da uva:");
+    	int uva = entrada.nextInt();
+    	
+    
+    	
+    	
+    	System.out.print("Resultado da soma = "+ (pera + uva) );
 	}
+	
 }
