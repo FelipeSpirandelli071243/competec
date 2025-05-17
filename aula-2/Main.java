4ba229c35d529a8a17e40e96d0dba38be1976723
FelipeSpirandelli071243 <f.spirandelli07@gmail.com>
2025-05-17 11:32:07 -0300

    Update Main.java

--- aula-2/Main.java
+++ aula-2/Main.java
@@ -1,7 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    Scanner entrada = new Scanner (System.in);
+       double enroladinhodesalsicha = 3.50;
+       double pastel = 10.00;
+       double coxinha = 4.00;
+       System.out.println("Oque você quer do menu ?");
+       String pedido = entrada.next();
+       System.out.println("Quantas unidades ?");
+       String unidades = entrada.next();
+       
 	}
 }
